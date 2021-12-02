3b862c0488570ccc18b94b982efb7d3b90d7c290
kosi2109 <nolockfacebook55@gmail.com>
2021-12-02 22:41:10 +0630

    final

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api/v1" });
+const API = axios.create({ baseURL: "https://kalli-sneaker.herokuapp.com/api/v1" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
